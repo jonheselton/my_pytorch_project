59f64e251ea77b4b43ecdebde026d535366767ea
jonheselton <git@github.com>
2024-05-04 23:42:23 -0500

    Changed the activation and loss functions.  broke the dataset into pieces

--- tutorial/diffusion_hf_01.py
+++ tutorial/diffusion_hf_01.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 from torch import nn
 from torch.nn import functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import numpy as np
@@ -30,7 +30,7 @@ class BasicUNet(nn.Module):
             nn.Conv2d(512, 256, kernel_size=5, padding=2),
             nn.Conv2d(256, out_channels, kernel_size=5, padding=2), 
         ])
-        self.act = nn.SiLU() # The activation function
+        self.act = nn.ReLU() # The activation function
         self.downscale = nn.MaxPool2d(2)
         self.upscale = nn.Upsample(scale_factor=2)
     def forward(self, x):
@@ -58,86 +58,106 @@ workers = 16
 batch_size = 32
 image_size = 256
 # Use celeb dataloader instead
-dataset = dset.ImageFolder(root=dataroot,
-                           transform=transforms.Compose([
+dataset = dset.ImageFolder(root=dataroot, transform=transforms.Compose([
                                transforms.Resize(image_size),
                                transforms.CenterCrop(image_size),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
-# Create the dataloader
+
+# create smaller subsets of the total data
+total_images = len(dataset)
+num_subsets = 9
+images_per_subset = int(total_images / num_subsets)
+all_subsets = []
+# Randomize the order
+all_indices = np.random.permutation(total_images)
+
+start_idx = 0
+for i in range(num_subsets):
+  subset_indices = all_indices[start_idx:start_idx + images_per_subset]
+  # Create the subset
+  current_subset = Subset(dataset, subset_indices)
+  all_subsets.append(current_subset)
+  start_idx += images_per_subset
+
+# Create the dataloaders
 # Dataloader contains a data tensor along with a tensor of classes{?comfirm?}
 # Dataloader length = batch size / data objects
-train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
-                                         shuffle=True, num_workers=workers)
+data_loaders = []
+for i in all_subsets:
+    subset_dataloader = DataLoader(i, batch_size=batch_size, shuffle=True, num_workers=workers)
+    data_loaders.append(subset_dataloader)
 # How many runs through the data should we do?
-n_epochs = 3
+n_epochs = 4
 
 # Create the network
 net = BasicUNet()
 net.to(device)
 
 # Our loss function
-loss_fn = nn.MSELoss()
+loss_fn = nn.SmoothL1Loss()
 
 # The optimizer
 opt = torch.optim.Adam(net.parameters(), lr=1e-3) 
 
 # Keeping a record of the losses for later viewing
-losses = []
+data_set_number = 0
+# The training loops
+for train_dataloader in data_loaders:
+    losses_subset = []
+    for epoch in range(n_epochs):
+        losses_epoch = []
+        i = 0
+        for x, y in train_dataloader:
+            i += 1
+            # Get some data and prepare the corrupted version
+            x = x.to(device) # Data on the GPU
+            noise_amount = torch.rand(x.shape[0]).to(device) # Pick random noise amounts
+            noisy_x = corrupt(x, noise_amount) # Create our noisy x
 
-# The training loop
-for epoch in range(n_epochs):
-    i = 0
-    for x, y in train_dataloader:
-        i += batch_size
-        # Get some data and prepare the corrupted version
-        x = x.to(device) # Data on the GPU
-        noise_amount = torch.rand(x.shape[0]).to(device) # Pick random noise amounts
-        noisy_x = corrupt(x, noise_amount) # Create our noisy x
+            # Get the model prediction
+            pred = net(noisy_x)
+            # Calculate the loss
+            loss = loss_fn(pred, x) # How close is the output to the true 'clean' x?
 
-        # Get the model prediction
-        pred = net(noisy_x)
-        # Calculate the loss
-        loss = loss_fn(pred, x) # How close is the output to the true 'clean' x?
+            # Backprop and update the params:
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
 
-        # Backprop and update the params:
-        opt.zero_grad()
-        loss.backward()
-        opt.step()
+            # Store the loss for later
+            losses_epoch.append(loss.item())
+            losses_subset.append(loss.item())
+            if i % 3200 == 0:
+                print('[%d/%d][%d/%d]\tLoss: %.4f'
+                % (epoch, n_epochs, i, len(train_dataloader),
+                    loss.item()))
 
-        # Store the loss for later
-        losses.append(loss.item())
-        if i % 100 == 0:
-            print('[%d/%d][%d/%d]\tLoss: %.4f'
-            % (epoch, n_epochs, i, len(train_dataloader),
-                loss.item()))
-
-    # Print our the average of the loss values for this epoch:
-    avg_loss = sum(losses[-len(train_dataloader):])/len(train_dataloader)
-    print(f'Finished epoch {epoch}. Average loss for this epoch: {avg_loss:05f}')
-#       Using 17 - 18GB VRAM     
+        # Print our the average of the loss values for this epoch:
+        avg_loss_epoch = sum(losses_epoch)/len(losses_epoch)
+        print(f'Finished dataset {data_set_number + 1}, epoch {epoch + 1}. Average loss for this epoch: {avg_loss_epoch:05f}')
+    avg_loss_subset = sum(losses_subset)/len(losses)
+    print(f'Finished dataset {data_set_number + 1}, Average loss for this subset {avg_loss_subset:05f}')
+    data_set_number += 1
 #     Results of each epoch during training
 #       Finished epoch 0. Average loss for this epoch: 0.638191
 #       Finished epoch 1. Average loss for this epoch: 0.366308
 #       Finished epoch 2. Average loss for this epoch: 0.366307
-
-# Finished epoch 0. Average loss for this epoch: 0.727461 - part II
-# Finished epoch 1. Average loss for this epoch: 0.366305
+#           Finished epoch 0. Average loss for this epoch: 0.727461 - part II
+#           Finished epoch 1. Average loss for this epoch: 0.366305
 # Save the model
-PATH = './models/unet_diffusion.pth'
+PATH = './models/subsets_unet_diffusion.pth'
 torch.save(net.state_dict(), PATH)
 # net = BasicUNet().to(device)
 #net.load_state_dict(torch.load(PATH))
 # Sampling
 n_steps = 40
-x = torch.rand(16, 3, 256, 256).to(device)
-print(f'Shape of noise - {x.shape}')
+x = torch.rand(4, 3, 256, 256).to(device)
 for i in range(n_steps):
-    noise_amount = torch.ones((x.shape[0], )).to(device) * (1-(i/n_steps)) # Starting high going low
     with torch.no_grad():
         pred = net(x)
     mix_factor = 1/(n_steps - i)
     x = x*(1-mix_factor) + pred*mix_factor
     for j, img in enumerate(pred): # j = num of fake images
-        save_image(img, f'generated_images/celeb_step_{i}_img_{j}.png')
+        save_image(img, f'generated_images/relu_celeb_step_{i}_img_{j}.png')
